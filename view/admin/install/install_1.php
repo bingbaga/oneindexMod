ec1583405e4f002dd96194e920cc903790fdc9ef
david <david@sha.vip>
2019-02-21 09:26:43 +0800

    修改回调地址可修改

--- view/admin/install/install_1.php
+++ view/admin/install/install_1.php
@@ -36,7 +36,7 @@
 		   <label class="mdui-textfield-label">由于你的网站不是<b>http://localhost/</b>。将通过ju.tn进行中转</label>
 		   <?php endif;?>
 		   <label class="mdui-textfield-label"><?php echo $redirect_uri;?></label>
-		   <input type="text" class="mdui-textfield-input" disabled  value="<?php echo $redirect_uri;?>"/>
+		   <input type="text" class="mdui-textfield-input" value="<?php echo $redirect_uri;?>"/>
 		   <input type="hidden" class="mdui-textfield-input" name="redirect_uri" value="<?php echo $redirect_uri;?>"/>
 		   <div class="mdui-textfield-error">重定向URL不能为空</div>
 		</div>
